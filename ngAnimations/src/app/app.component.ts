5f46e36ceba38c1791f6ddcfa9bc6646821f27e0
Mayssa Belkamel <2483601@cegepmontpetit.ca>
2026-05-15 16:30:42 -0400

    FCT: debut animation

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -1,16 +1,33 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component } from '@angular/core';
-import { bounce, shakeX, tada } from 'ng-animate';
+import { bounce, shake, shakeX, tada } from 'ng-animate';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    standalone: true
+    standalone: true,
+    animations: [
+    trigger('shake', [transition('* => *', useAnimation(shake, {
+      // Set the duration to 3 seconds and delay to 1 second
+      params: { timing: 2, delay: 0 }
+    }))]),
+    trigger('bounce', [transition('* => *', useAnimation(bounce,{
+       params: { timing: 4, delay: 2 }
+    }))]),
+    trigger('tada', [transition('* => *', useAnimation(tada,{
+       params: { timing: 3, delay: 5 }
+    }))])
+],
 })
 export class AppComponent {
   title = 'ngAnimations';
-
+    mavariable=0;
+ // shake= false;
+bounce=false;
   constructor() {
   }
+
+ 
+  
 }
