068a51d070a41a854f4d206393996e43d16352ac
MayssaBelkaml <2483601@cegepmontpetit.ca>
2026-05-24 19:32:25 -0400

    fct:fini question sur annimation

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component } from '@angular/core';
 import { bounce, shake, shakeX, tada } from 'ng-animate';
+import { lastValueFrom, timer } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -8,26 +9,83 @@ import { bounce, shake, shakeX, tada } from 'ng-animate';
     styleUrls: ['./app.component.css'],
     standalone: true,
     animations: [
-    trigger('shake', [transition('* => *', useAnimation(shake, {
-      // Set the duration to 3 seconds and delay to 1 second
-      params: { timing: 2, delay: 0 }
-    }))]),
-    trigger('bounce', [transition('* => *', useAnimation(bounce,{
-       params: { timing: 4, delay: 2 }
-    }))]),
-    trigger('tada', [transition('* => *', useAnimation(tada,{
-       params: { timing: 3, delay: 5 }
-    }))])
+    trigger('shake', [
+    transition(':increment', useAnimation(shake, { params: { timing: 2 } }))
+  ]),
+  trigger('bounce', [
+    transition(':increment', useAnimation(bounce, { params: { timing: 4 } }))
+  ]),
+  trigger('tada', [
+    transition(':increment', useAnimation(tada, { params: { timing: 3 } }))
+  ])
 ],
 })
 export class AppComponent {
   title = 'ngAnimations';
-    mavariable=0;
- // shake= false;
-bounce=false;
+  ng_shake = 0;
+ng_bounce = 0;
+ng_tada = 0;
+
+css_rotate = false;
+
+
+
+
   constructor() {
   }
 
  
-  
+  waitFor(seconds: number) {
+  return lastValueFrom(timer(seconds * 1000));
+}
+
+async playOnce() {
+
+  // 1) Shake rouge 2 sec
+  this.ng_shake++;
+  await this.waitFor(2);
+
+  // 2) Bounce vert 4 sec
+  this.ng_bounce++;
+
+  // 3) Tada bleu → commence 1 sec avant la fin du bounce
+  await this.waitFor(3); // 4 sec - 1 sec
+  this.ng_tada++;
+
+  // Attendre la fin du tada (3 sec)
+  await this.waitFor(3);
+}
+
+loop = false;
+
+async playLoop() {
+  this.loop = true;
+
+  while (this.loop) {
+
+    this.ng_shake++;
+    await this.waitFor(2);
+
+    this.ng_bounce++;
+    await this.waitFor(3);
+
+    this.ng_tada++;
+    await this.waitFor(3);
+  }
+}
+
+stopLoop() {
+  this.loop = false;
+}
+
+
+playRotate() {
+  this.css_rotate = true;
+
+  setTimeout(() => {
+    this.css_rotate = false;
+  }, 2000); // même durée que ton animation CSS
+}
+
+
 }
